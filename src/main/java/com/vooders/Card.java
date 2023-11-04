46acde707cdb61ba8f1d0ba7c57b4fbae1a00185
Kev Wilson <kev.wilson@bbc.co.uk>
2023-11-04 17:13:52 +0000

    Adds Card class
    Adds a class to represent a card.
    
    As well as holding the standard card attibutes faceValue, suit and value
    Cards are also comparible to other cards.
    
    There's an equals() method which defines equality
    
    It also implements the Comparable interface which forces us to implement compareTo
    compareTo() defines how a Card should be compared to another Card.
    
    Using these methods Cards can be sorted in collections or directly
    compared to other Cards using isGreaterThan or is isLowerThan.
    
    The not loads of code here but there's a few concepts going on for you
    to have a play with and learn about.

---
+++ src/main/java/com/vooders/Card.java
@@ -0,0 +1,58 @@
+package com.vooders;
+
+import java.util.Objects;
+
+public class Card implements Comparable<Card> {
+    private final String faceValue;
+    private final int value;
+    private final String suit;
+
+    public Card(String faceValue, String suit, int value) {
+        this.faceValue = faceValue;
+        this.value = value;
+        this.suit = suit;
+    }
+
+    public String getFaceValue() {
+        return this.faceValue;
+    }
+
+    public String getSuit() {
+        return this.suit;
+    }
+
+    public int getValue() {
+        return this.value;
+    }
+
+    public boolean isGreaterThan(Card card) {
+        return this.compareTo(card) > 0;
+    }
+
+    public boolean isLowerThan(Card card) {
+        return this.compareTo(card) < 0;
+    }
+
+    public String toString() {
+        return this.faceValue + this.suit;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Card card = (Card) o;
+        return value == card.value;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
+    }
+
+    @Override
+    public int compareTo(Card card) {
+        int compareQuantity = card.getValue();
+        return this.value - compareQuantity;
+    }
+}
---
+++ src/test/java/com/vooders/CardTest.java
@@ -0,0 +1,62 @@
+package com.vooders;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CardTest {
+    @Test
+    void a_card_has_a_face_value() {
+        String faceValue = "4";
+        Card card = new Card(faceValue, "♣", 1);
+
+        assertEquals(faceValue, card.getFaceValue());
+    }
+
+    @Test
+    void a_card_has_a_suit() {
+        String suit = "♣";
+        Card card = new Card("K", suit, 1);
+
+        assertEquals(suit, card.getSuit());
+    }
+
+    @Test
+    void a_card_has_a_value() {
+        int value = 4;
+        Card card = new Card("4", "♣" , value);
+
+        assertEquals(value, card.getValue());
+    }
+
+    @Test
+    void a_high_card_is_greater_than_a_lower_card() {
+        Card highCard = new Card("2","♣", 2);
+        Card lowCard = new Card("4", "♣", 4);
+
+        assertTrue(lowCard.isGreaterThan(highCard));
+    }
+
+    @Test
+    void a_low_card_is_lower_than_a_high_card() {
+        Card highCard = new Card("2","♣", 2);
+        Card lowCard = new Card("4", "♣", 4);
+
+        assertTrue(highCard.isLowerThan(lowCard));
+    }
+
+    @Test
+    void cards_with_the_same_value_are_equal() {
+        Card lowCard = new Card("K", "♣",  10);
+        Card highCard = new Card("10", "♣", 10);
+
+        assertEquals(highCard, lowCard);
+    }
+
+    @Test
+    void card_string_value_should_be_faceValue_and_suit() {
+        Card card = new Card("K", "♣",  10);
+
+        assertEquals("K♣", card.toString());
+    }
+}
