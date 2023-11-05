b63fd2e3eb7b1da450caadd359d73df2f320fe49
Kev Wilson <kev.wilson@bbc.co.uk>
2023-11-05 00:39:21 +0000

    Adds Hand class
    A hand starts with two cards and has the ability to add more.
    Has an `isBust()` method which indicates whether the hand total is over 21
    Has an `canSplit()` method to indicate whether the hand can be split
    
    Hands use the total of their Cards too determine equality and to compare
    themselves with other Hands

---
+++ src/main/java/com/vooders/Hand.java
@@ -0,0 +1,73 @@
+package com.vooders;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class Hand implements Comparable<Hand> {
+
+    private final List<Card> cards;
+    private static final int BUST_LIMIT = 21;
+
+    public Hand(Card fistCard, Card secondCard) {
+        cards = new ArrayList<>();
+        this.cards.add(fistCard);
+        this.cards.add(secondCard);
+    }
+
+    public List<Card> getCards() {
+        return cards;
+    }
+
+    public int getTotal() {
+        return cards.stream()
+                .mapToInt(Card::getValue)
+                .sum();
+    }
+
+    public void addCard(Card card) {
+        this.cards.add(card);
+    }
+
+    public boolean isBust() {
+        return this.getTotal() > BUST_LIMIT;
+    }
+
+    public boolean canSplit() {
+        if (cards.size() != 2) {
+            return false;
+        }
+        return cards.get(0).getFaceValue().equals(cards.get(1).getFaceValue());
+    }
+
+    public boolean isGreaterThan(Hand hand) {
+        return this.compareTo(hand) > 0;
+    }
+
+    public boolean isLowerThan(Hand hand) {
+        return this.compareTo(hand) < 0;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Hand hand = (Hand) o;
+        return getTotal() == hand.getTotal();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(cards);
+    }
+
+    @Override
+    public int compareTo(Hand otherHand) {
+        return getTotal() - otherHand.getTotal();
+    }
+
+    @Override
+    public String toString() {
+        return cards.toString();
+    }
+}
---
+++ src/test/java/com/vooders/HandTest.java
@@ -0,0 +1,118 @@
+package com.vooders;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HandTest {
+    @Test
+    void a_hand_total_is_the_sum_of_the_cards() {
+        Card fistCard = new Card("K", "s", 10);
+        Card secondCard = new Card("K", "s", 10);
+        Hand hand = new Hand(fistCard, secondCard);
+
+        assertEquals(20, hand.getTotal());
+    }
+
+    @Test
+    void is_not_bust_if_total_is_less_than_21() {
+        Card fistCard = new Card("K", "s", 10);
+        Card secondCard = new Card("K", "s", 10);
+        Hand hand = new Hand(fistCard, secondCard);
+
+        assertFalse(hand.isBust());
+    }
+
+    @Test
+    void can_add_a_card_to_a_hand() {
+        Hand hand = new Hand(
+                new Card("K", "s", 10),
+                new Card("K", "s", 10));
+
+        hand.addCard(new Card("K", "s", 10));
+
+        assertEquals(3, hand.getCards().size());
+    }
+
+    @Test
+    void is_bust_if_total_is_over_21() {
+        Card fistCard = new Card("K", "s", 30);
+        Card secondCard = new Card("K", "s", 10);
+        Hand hand = new Hand(fistCard, secondCard);
+
+        assertTrue(hand.isBust());
+    }
+
+    @Test
+    void can_split_if_first_two_cards_have_the_same_faceValue() {
+        Card fistCard = new Card("K", "s", 10);
+        Card secondCard = new Card("K", "s", 10);
+        Hand hand = new Hand(fistCard, secondCard);
+
+        assertTrue(hand.canSplit());
+    }
+
+    @Test
+    void cant_split_a_hand_with_more_than_2_cards() {
+        Hand hand = new Hand(
+                new Card("K", "s", 10),
+                new Card("K", "s", 10));
+
+        hand.addCard(new Card("K", "s", 10));
+
+        assertFalse(hand.canSplit());
+    }
+
+    @Test
+    void hands_with_the_same_total_are_equal() {
+        Hand hand1 = new Hand(
+                new Card("K", "s", 10),
+                new Card("K", "s", 10));
+
+        Hand hand2 = new Hand(
+                new Card("10", "s", 10),
+                new Card("Q", "h", 10));
+
+        assertEquals(hand1, hand2);
+    }
+
+    @Test
+    void a_hand_with_a_higher_total_is_greater_than_one_with_a_lower_total() {
+        Hand hand1 = new Hand(
+                new Card("K", "s", 10),
+                new Card("K", "s", 2));
+
+        Hand hand2 = new Hand(
+                new Card("10", "s", 10),
+                new Card("Q", "h", 10));
+
+        assertTrue(hand2.isGreaterThan(hand1));
+        assertFalse(hand1.isGreaterThan(hand2));
+    }
+
+    @Test
+    void a_hand_with_a_lower_total_is_lower_than_one_with_a_higher_total() {
+        Hand hand1 = new Hand(
+                new Card("K", "s", 10),
+                new Card("K", "s", 2));
+
+        Hand hand2 = new Hand(
+                new Card("10", "s", 10),
+                new Card("Q", "h", 10));
+
+        assertTrue(hand1.isLowerThan(hand2));
+        assertFalse(hand2.isLowerThan(hand1));
+    }
+
+    @Test
+    void hand_string_value_should_be_all_its_cards_string_values() {
+        Hand hand = new Hand(
+                new Card("10", "s", 10),
+                new Card("Q", "h", 10));
+        hand.addCard(new Card("K", "s", 10));
+
+        String expectedString = "[10s, Qh, Ks]";
+
+        assertEquals(expectedString, hand.toString());
+    }
+}
